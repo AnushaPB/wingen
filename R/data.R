42fe78fa3d1346a79c5d7d17b5fda4cae1047a78
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-03 19:55:56 -0700

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Example vcf
+#'
+#' A Variant Call Format data set
+#'
+#'
+#' @format Object of class vcfR with 100 individuals and 1000 loci
+#' @source
+"ex_vcf"
+
+#' Example coordinates
+#'
+#' A data set of coordinates
+#'
+#'
+#' @format A data frame with 100 rows and 2 columns
+#' \describe{
+#'   \item{x}{x coordinate}
+#'   \item{y}{y coordinate}
+#' }
+#' @source
+"ex_coords"
+
+#' Example raster
+#'
+#' RasterLayer of California
+#'
+#'
+#' @format A RasterLayer with uniform values of 1
+#' @source
+"ex_lyr"
