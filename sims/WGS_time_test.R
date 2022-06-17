71c7760a90a6877de25a3e0555e68aaf6f6a1ff1
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-16 17:29:33 -0700

    Update WGS_time_test.R

--- sims/WGS_time_test.R
+++ sims/WGS_time_test.R
@@ -9,16 +9,15 @@ library(hierfstat)
 library(here)
 source(here("sims/sim_functions.R"))
 
-vcf <- read.vcfR("data/mod-sim_params_it-0_t-500_spp-spp_0.vcf")
-geo <- read.csv("data/mod-sim_params_it-0_t-500_spp-spp_0.csv")
+vcf <- read.vcfR(here("sims/data/mod-sim_params_it-0_t-500_spp-spp_0.vcf"))
+geo <- read.csv(here("sims/data/mod-sim_params_it-0_t-500_spp-spp_0.csv"))
 coords <- geo[,c("idx","x","y")]
+IDS <- read.csv(here("sims/data/samples_seed42.csv"))
 
-set.seed(42)
+# get indexes of individuals
+si <- IDS$inds
+# sample loci
 l <- sample(nrow(vcf@gt), 100000)
-#s <- grid_samp(coords, npts = 350, ldim = 100)
-#s <- s[1:200]
-#si <- which(coords$idx %in% s)
-si <- sample(nrow(coords), 200)
 
 subvcf <- vcf[l,c(1, si+1)]
 
@@ -40,10 +39,11 @@ registerDoParallel(cl)
 
 results <- purrr::map(c("het", "pi", "allelic.richness"), time_test, "stat", subvcf, subcoords, lyr, fact = 3, wdim = 5, rarify_n = 4, rarify_nit = 5, parallel = TRUE, nloci = nrow(subvcf@gt))
 
-stopCluster(cl)
-
-save(results, file = "results_WGS_10p.RData")
+save(results, file = "results_WGS_10p_200ind.RData")
 
 resls <- unlist_test(results)
-write.csv(resls$df, "time_results_WGS_10p.csv")
-terra::writeRaster(terra::rast(resls$raster), "results_WGS.tif")
+write.csv(resls$df, "time_results_WGS_10p_200ind.csv")
+write_rast_test(results, "WGS_200ind")
+
+
+stopCluster(cl)
