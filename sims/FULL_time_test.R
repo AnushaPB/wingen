7be47134cedf9a5e3bca82b9b9f6e9bcf31cd500
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-17 18:33:54 -0700

    Update FULL_time_test.R

--- sims/FULL_time_test.R
+++ sims/FULL_time_test.R
@@ -25,17 +25,28 @@ cores <- 10
 cl <- makeCluster(cores)
 registerDoParallel(cl)
 
-#results <- purrr::map(c("het", "pi", "allelic.richness"), time_test, "stat", vcf, coords, lyr, fact = 3, wdim = 5,  rarify = FALSE, min_n = 4, parallel = TRUE, nloci = nrow(vcf@gt))
+stats <- c("pi", "het", "allelic.richness")
+for(i in 1:length(stats)){
+  ptm <- Sys.time()
+  gdmapr <- window_gd(vcf, coords, lyr, stat = stats[[i]], fact = 3, wdim = 5, rarify = TRUE, rarify_n = 4, rarify_nit = 5, min_n = 4, fun = mean, parallel = TRUE, nloci = nrow(vcf@gt))
 
-#save(results, file = "results_FULL2.RData")
+  df <- data.frame(time = (Sys.time() - ptm),
+                   fact = 3,
+                   wdim = 5,
+                   rarify_n = 4,
+                   rarify_nit = 5)
 
-results <- purrr::map(c("het", "pi", "allelic.richness"), time_test, "stat", vcf, coords, lyr, fact = 3, wdim = 5, rarify_n = 4, rarify_nit = 5, parallel = TRUE, nloci = nrow(vcf@gt))
+  results[[i]] <- list(df, gdmapr)
+
+  # temp: see results as they get output
+  write_rast_test(results, here("sims/outputs/FULL"))
+}
+
+save(results, file = here("sims/outputs/FULL_rarify.RData"))
+
 resls <- unlist_test(results)
-write.csv(resls$df, "time_results_FULL.csv")
-terra::writeRaster(terra::rast(resls$raster), "results_FULL.tif")
-
-#results <- purrr::map(c("het", "pi", "allelic.richness"), time_test, "stat", vcf, coords, lyr, fact = 3, wdim = 5, rarify_n = 4, rarify_nit = 5, parallel = TRUE, nloci = nrow(vcf@gt))
-save(results, file = "results_FULL_rarify.RData")
+write.csv(resls$df, here("sims/outputs/time_results_FULL.csv"))
+write_rast_test(results, here("sims/outputs/FULL"))
 
 
 stopCluster(cl)
