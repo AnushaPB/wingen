5111014b726f4e68b6101634e6f24b7c40b117df
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-17 18:33:49 -0700

    reorganize files

---
+++ tests/testthat/test_window_gd.R
@@ -0,0 +1,24 @@
+
+
+test_that("wdim_check fixes wdim", {
+  expect_error(wdim_check(1))
+  expect_error(wdim_check(c(2, 3)))
+
+  expect_equal(wdim_check(c(3, 3)), c(3, 3))
+  expect_warning(expect_equal(wdim_check(c(3, 4)), c(3, 5)))
+  expect_warning(expect_equal(wdim_check(c(4, 3)), c(5, 3)))
+  expect_warning(expect_warning(expect_equal(wdim_check(c(4, 4)), c(5, 5))))
+
+  expect_warning(wdim_check(c(3, 4)))
+  expect_warning(wdim_check(4))
+})
+
+test_that("biallelic richness is calculated correctly", {
+  expected <- c(1, 2, 2, 2, 2, 2, 2, 2, 1)
+  all_possible_combos <- t(expand.grid(0:2, 0:2))
+  ar_vals <- apply(all_possible_combos, 2, helper_calc_biar)
+  expect_equal(ar_vals, expected)
+  expect_equal(calc_mean_biar(all_possible_combos), mean(expected))
+
+  expect_error(calc_mean_biar(matrix(c(0:4), nrow = 1)), "to calculate biallelic richness, all values in genetic matrix must be 0, 1 or 2")
+})
