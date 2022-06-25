45df921a1efa942ab5d15a1dfa7c229531b6030b
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-25 08:56:08 -0700

    Add middle earth data

--- R/data.R
+++ R/data.R
@@ -28,3 +28,30 @@
 #' @format A RasterLayer with uniform values of 1
 #' @source
 "ex_lyr"
+
+#' Middle earth example coordinates
+#'
+#' @format A data frame with 100 rows and 2 columns
+#' \describe{
+#'   \item{x}{x coordinate}
+#'   \item{y}{y coordinate}
+#' }
+#' @source
+"middle_earth_coords"
+
+#' Middle earth example vcf
+#'
+#' A Variant Call Format data set
+#'
+#' @format Object of class vcfR with 100 individuals and 1000 loci
+#' @source
+"middle_earth_vcf"
+
+#' Middle earth example raster
+#'
+#' RasterLayer of middle earth from CITE
+#'
+#'
+#' @format A RasterLayer of middle earth
+#' @source
+"middle_earth_lyr"
