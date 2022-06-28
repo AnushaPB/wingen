0ee242ce9f7188a4b5081c488a503fdff3cb20f3
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-28 14:59:08 -0700

    update krige tests

--- R/krig_gd.R
+++ R/krig_gd.R
@@ -15,7 +15,7 @@
 #' plot_count(kpi)
 #'
 
-krig_gd <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, agg = NULL, disagg = NULL, n_cell = 10000){
+krig_gd <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, agg_grd = NULL, disagg_grd = NULL, agg_r = NULL, disagg_r = NULL, resample_first = TRUE, n_cell = 10000){
 
   rls <- raster::as.list(r)
 
@@ -24,7 +24,7 @@ krig_gd <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, 
     warning("no grd provided, defaults to using first raster layer to create grd")
   }
 
-  rstk <- purrr::map(rls, krig_gd_lyr, grd, coords, xy, resample, agg, disagg, n_cell)
+  rstk <- purrr::map(rls, krig_gd_lyr, grd, coords, xy, resample, agg_grd, disagg_grd, agg_r, disagg_r, n_cell)
   rstk <- raster::stack(rstk)
 
   names(rstk) <- names(r)
@@ -40,9 +40,12 @@ krig_gd <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, 
 #' @param grd object to create grid for kriging, can be RasterLayer, SpatialPointsDataFrame, or a gridded object as defined by 'sp'. If undefined, will use \code{r} to create a grid.
 #' @param coords if provided, kriging will occur based only on values at these coordinates
 #' @param xy whether to co-krige with x and y (~x+y)
-#' @param resample if grd is a raster, whether to resample r to the resolution of grd (defaults to FALSE)
-#' @param agg factor used for aggregation of grd if provided
-#' @param disagg factor used for disaggregation of grd if provided
+#' @param resample whether to resample grd or r. Set to "r" to resample r to grd Set to "grd" to resample grd to r. Defaults to FALSE but we *highly recommend setting this to either "grd" or "r"*
+#' @param agg_grd factor to use for aggregation of grd, if provided
+#' @param disagg_grd factor to use for disaggregation of grd, if provided
+#' @param agg_r factor to use for aggregation of r, if provided
+#' @param disagg_r factor to use for disaggregation, of r if provided
+#' @param resample_first if aggregation or disaggregation is used in addition to resampling, whether to resample before (resample_first = TRUE) or after (resample_first = FALSE) aggregation/disaggregation (defaults to TRUE)
 #' @param n_cell number of cells to interpolate across if SpatialPointsDataFrame is provided for \code{grd}
 #'
 #' @return RasterLayer
@@ -51,11 +54,13 @@ krig_gd <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, 
 #' @keywords internal
 #'
 #' @examples
-krig_gd_lyr <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, agg = NULL, disagg = NULL, n_cell = 1000) {
+krig_gd_lyr <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FALSE, agg_grd = NULL, disagg_grd = NULL, agg_r = NULL, disagg_r = NULL, resample_first = TRUE, n_cell = 1000) {
 
-  # resample raster layer to grd resolution if grd is a raster and resample = TRUE
-  if(class(grd) == "RasterLayer" & resample){
-    r <- raster::resample(r, grd)
+  # Transform raster layer
+  if(class(grd) == "RasterLayer"){
+    stk <- raster_transform(r, grd)
+    r <- stk[[names(r)]]
+    grd <- stk[["grd"]]
   }
 
   # convert raster to df
@@ -68,12 +73,12 @@ krig_gd_lyr <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FAL
 
   # create grid
   if(is.null(grd)){
-    krig_grid <- raster_to_grid(r, agg = agg, disagg = disagg)
+    krig_grid <- raster_to_grid(r)
   } else if(class(grd) == "SpatialPointsDataFrame") {
     #TODO: FIX THIS NOT WORKING
     krig_grid <- spdf_to_grid(grd, n_cell = n_cell)
   } else if(class(grd) == "RasterLayer"){
-    krig_grid <- raster_to_grid(grd, agg = agg, disagg = disagg)
+    krig_grid <- raster_to_grid(grd)
   } else if(sp::gridded(grd)){
     krig_grid <- grd
   } else { stop(" unable to find an inherited method for type of grd provided")}
@@ -120,10 +125,7 @@ krig_gd_lyr <- function(r, grd = NULL, coords = NULL, xy = FALSE, resample = FAL
 #' @keywords internal
 #'
 #' @examples
-raster_to_grid <- function(x, agg = NULL, disagg = NULL){
-  if(!is.null(agg)){x <- raster::aggregate(x, agg)}
-  if(!is.null(disagg)){x <- raster::disaggregate(x, disagg)}
-
+raster_to_grid <- function(x){
   grd <- data.frame(raster::rasterToPoints(x))
   sp::coordinates(grd) <- ~ x + y
   sp::gridded(grd) <- TRUE
@@ -160,3 +162,39 @@ spdf_to_grid <- function(spdf, n_cell = 1000) {
   return(krig_grd)
 }
 
+
+#' Transform raster
+#'
+#' @inheritParams krig_gd
+#'
+#' @return stack of transformed rasters
+#' @export
+#'
+#' @examples
+raster_transform <- function(r, grd, resample = FALSE, agg_grd = NULL, disagg_grd = NULL, agg_r = NULL, disagg_r = NULL, resample_first = TRUE){
+  if(nlayers(r) > 1) stop(">1 layer provided for r")
+  if(nlayers(grd) > 1) stop(">1 layer provided for grd")
+
+  if(resample_first){
+    if(resample == "r") r <- raster::resample(r, grd)
+    if(resample == "grd") grd <- raster::resample(grd, r)
+  }
+
+  if(!is.null(agg_grd) & !is.null(disagg_grd)) stop("Both agg_grd and disagg_grd provided, when only one should be provided")
+  if(!is.null(agg_grd)) grd <- raster::aggregate(grd, agg_grd)
+  if(!is.null(disagg_grd)) grd <- raster::disaggregate(grd, disagg_grd)
+
+  if(!is.null(agg_r) & !is.null(disagg_r)) stop("Both agg_r and disagg_r provided, when only one should be provided")
+  if(!is.null(agg_r)) r <- raster::aggregate(r, agg_r)
+  if(!is.null(disagg_r)) r <- raster::disaggregate(r, disagg_r)
+
+  if(!resample_first){
+    if(resample == "r") r <- raster::resample(r, grd)
+    if(resample == "grd") grd <- raster::resample(grd, r)
+  }
+
+  s <- list(r, grd)
+  names(s) <- c(names(r), "grd")
+
+  return(s)
+}
--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -2,6 +2,6 @@ test_that("incorrect inputs to plot produces correct errors", {
   data("mini_lyr")
   bad_stack <- raster::stack(mini_lyr, mini_lyr, mini_lyr)
 
-  expect_error(plot_gd(bad_stack))
-  expect_error(plot_count(bad_stack))
+  expect_warning(plot_gd(bad_stack))
+  expect_warning(plot_count(bad_stack))
 })
