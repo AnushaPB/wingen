ad2f585555f33ae7d5ca9bb2e34fdf941d3da672
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-06-28 15:58:26 -0700

    Create varyn_time_test.R

---
+++ sims/time_tests/varyn_time_test.R
@@ -0,0 +1,30 @@
+library(wingen)
+library(foreach)
+library(doParallel)
+library(here)
+source(here("sims/sim_functions.R"))
+
+set.seed(42)
+
+# load vcf, coords, and lyr
+load_middle_earth()
+coords <- coords[,c("x","y")]
+
+# check match
+s <- sample(1:nrow(coords), 500)
+vcf <- vcf[, c(1, s + 1)]
+coords <- coords[, s]
+stopifnot(colnames(vcf@gt)[-1] == as.character(coords$idx))
+
+# confirm that correct set is being used
+message(paste("nloci", nrow(vcf@gt), "/ nind", nrow(coords)))
+
+cores <- 10
+cl <- makeCluster(cores)
+registerDoParallel(cl)
+
+run_default_time_test(vcf, coords, lyr, rarify = TRUE, parallel = TRUE, file.name = "FULL")
+
+run_default_time_test(vcf, coords, lyr, rarify = FALSE, parallel = TRUE, file.name = "FULL")
+
+stopCluster(cl)
