73904e96cec599df3c6a8d9028723d3d385a3462
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-23 12:06:59 -0700

    create diagonal bottleneck

--- sims/make_bottleneck_lyr.R
+++ sims/make_bottleneck_lyr.R
@@ -1,14 +1,28 @@
 lyr <- read.csv(here("sims", "data", "middle_earth.csv"), header = FALSE)
-lyr <- raster(as.matrix(lyr))
-extent(lyr) <- extent(0,100,-100,0)
 
-bottleneck_lyr <- lyr/2
+# increase initial pop size
+lyr_init <- lyr*2
+lyr_init[lyr_init > 1] <- 1
 
+# on diagonal decrease pop size
+lyr_bc <- lyr_init
+tri <- lower.tri(lyr_bc)
+tri <- tri[, ncol(tri):1]
+lyr_bc[tri] <- init_lyr[tri]/2
 
-write.table(as.matrix(bottleneck_lyr), here("sims", "data", "bottleneck_lyr.csv"), sep = ",", row.names = FALSE, col.names = FALSE)
+write.table(as.matrix(lyr_bc), here("sims", "data", "bottleneck_lyr.csv"), sep = ",", row.names = FALSE, col.names = FALSE)
 lyr_bc <- read.csv( here("sims", "data", "bottleneck_lyr.csv"), header = FALSE)
 lyr_bc <- raster(as.matrix(lyr_bc))
 extent(lyr_bc) <- extent(0,100,-100,0)
 
-plot(lyr, zlim = c(0,1))
-plot(lyr_bc, zlim = c(0,1))
+write.table(as.matrix(init_lyr), here("sims", "data", "init_lyr.csv"), sep = ",", row.names = FALSE, col.names = FALSE)
+lyr_init <- read.csv( here("sims", "data", "init_lyr.csv"), header = FALSE)
+lyr_init <- raster(as.matrix(lyr_init))
+extent(lyr_init) <- extent(0,100,-100,0)
+
+plot(lyr_init, zlim = c(0,1), col = magma(100))
+plot(lyr_bc, zlim = c(0,1), col = magma(100))
+
+cellStats(lyr_init, sum)
+cellStats(lyr_bc, sum)
+cellStats(lyr_init, sum)/cellStats(lyr_bc, sum)
