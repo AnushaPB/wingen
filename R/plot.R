1f33028a38782ff7e3a6498e9ac009754bc602b9
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-26 16:58:50 -0700

    add legend to plot

--- R/plot.R
+++ R/plot.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-plot_gd <- function(x, bkg = NULL, col = viridis::magma(100), zlim = NULL, main = NULL){
+plot_gd <- function(x, bkg = NULL, col = viridis::magma(100), zlim = NULL, main = NULL, legend = TRUE){
 
   # suppress annoying and irrelevant plot warnings
   suppressWarnings({
@@ -18,9 +18,9 @@ plot_gd <- function(x, bkg = NULL, col = viridis::magma(100), zlim = NULL, main 
     if(!is.null(bkg)) {
       raster::plot(x[[1]], col = "white", legend = FALSE, main = main, axes = FALSE, box = FALSE)
       raster::plot(bkg, col = "lightgray", border = "white", axes = FALSE, box = FALSE, add = TRUE, legend = FALSE)
-      raster::plot(x[[1]], col = col, zlim = zlim, add = TRUE, axes = FALSE, box = FALSE)
+      raster::plot(x[[1]], col = col, zlim = zlim, add = TRUE, axes = FALSE, box = FALSE, legend = legend)
     } else {
-      raster::plot(x[[1]], col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE)
+      raster::plot(x[[1]], col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE, legend = legend)
     }
   }
   )
@@ -37,14 +37,14 @@ plot_gd <- function(x, bkg = NULL, col = viridis::magma(100), zlim = NULL, main 
 #' @export
 #'
 #' @examples
-plot_count <- function(x, col = viridis::mako(100), zlim = NULL, main = NULL){
+plot_count <- function(x, col = viridis::mako(100), zlim = NULL, main = NULL, legend = legend){
 
   # suppress annoying and irrelevant plot warnings
   suppressWarnings({
 
   if(nlayers(x) > 2) warning("More than two raster layers in stack provided, using second layer")
-  if(nlayers(x) == 2) raster::plot(x[[2]], col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE)
-  if(nlayers(x) == 1) raster::plot(x, col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE)
+  if(nlayers(x) == 2) raster::plot(x[[2]], col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE, legend = legend)
+  if(nlayers(x) == 1) raster::plot(x, col = col, zlim = zlim, main = main, axes = FALSE, box = FALSE, legend = legend)
 
   })
 }
