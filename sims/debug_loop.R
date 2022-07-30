707816f5c841aab8336ff1afdb45b77da2241c45
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-30 11:35:21 -0700

    CLEANUP SIMS FOLDER

--- sims/debug_loop.R
+++
@@ -1,5 +0,0 @@
-rast_vals <- purrr::map_dfr(1:raster::ncell(lyr), window_helper, lyr, gen, coord_cells, nmat, stat, rarify, rarify_n, rarify_nit, min_n, fun, nloci)
-
-for(i in 1:raster::ncell(lyr)){
-  window_helper(i, lyr, gen, coord_cells, nmat, stat, rarify, rarify_n, rarify_nit, min_n, fun, nloci)
-}
