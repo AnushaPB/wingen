85f666f3083caaa49b5e366faaf591cf0bea8a32
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-29 22:56:03 -0700

    update default args

--- R/window_gd.R
+++ R/window_gd.R
@@ -99,7 +99,7 @@ window_gd <- function(vcf, coords, lyr, stat = "pi", wdim = 5, fact = 0, rarify 
 #'
 #' @examples
 #'
-window_gd_general <- function(gen, coords, lyr, stat = calc_mean_ar, wdim = 5, fact = 0, rarify = FALSE, rarify_n = 4, rarify_nit = 10, min_n = 2, fun = mean, parallel = FALSE, nloci = NULL) {
+window_gd_general <- function(gen, coords, lyr, stat = calc_mean_ar, wdim = 3, fact = 0, rarify = FALSE, rarify_n = 2, rarify_nit = 10, min_n = 2, fun = mean, parallel = FALSE, nloci = NULL) {
   # format coords
   coords <- data.frame(coords)
   colnames(coords) <- c("x", "y")
