6c783cf53508c680059732c3f203f2d0b2ea394f
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-01 01:42:39 -0700

    add resolution arg

--- R/coords_to_raster.R
+++ R/coords_to_raster.R
@@ -2,6 +2,7 @@
 #'
 #' @param coords coordinates (two columns, the first should be x and the second should be y)
 #' @param buffer buffer to add to edge of raster (defaults to 0)
+#' @param res desired resolution of raster (defaults to NULL). Can be a single value for square cells or a vector with two values representing x and y resolutions.
 #' @param agg aggregation factor to apply to raster (defaults to NULL)
 #' @param disagg disaggregation factor to apply to raster (defaults to NULL)
 #' @param plot whether to plot resulting raster with coords (defaults to FALSE)
@@ -11,8 +12,8 @@
 #'
 #' @examples
 #' load_mini_ex()
-#' coords_to_raster(mini_coords, buffer = 10, plot = TRUE)
-coords_to_raster <- function(coords, buffer = 0, agg = NULL, disagg = NULL, plot = FALSE){
+#' coords_to_raster(mini_coords, buffer = 1, plot = TRUE)
+coords_to_raster <- function(coords, buffer = 0, res = NULL, agg = NULL, disagg = NULL, plot = FALSE){
 
   # format coords
   coords <- as.data.frame(coords)
@@ -25,7 +26,22 @@ coords_to_raster <- function(coords, buffer = 0, agg = NULL, disagg = NULL, plot
   ymax <- max(coords$y, na.rm = TRUE) + buffer
 
   # make a matrix
-  m <- matrix(nrow = (ymax - ymin), ncol = (xmax - xmin))
+  if(is.null(res)){
+    nrow = (ymax - ymin)
+    ncol = (xmax - xmin)
+  } else {
+    if(length(res) == 1){
+      nrow <- (ymax - ymin)/res
+      ncol <- (xmax - xmin)/res
+    } else if (length(res) == 2) {
+      nrow <- (ymax - ymin)/(res[1])
+      ncol <- (xmax - xmin)/(res[2])
+    } else {
+      stop("invalid res provided")
+    }
+  }
+
+  m <- matrix(nrow = nrow, ncol = ncol)
 
   # turn into raster
   r <- raster::raster(m)
@@ -34,6 +50,7 @@ coords_to_raster <- function(coords, buffer = 0, agg = NULL, disagg = NULL, plot
   raster::extent(r) <- c(xmin, xmax, ymin, ymax)
 
   # aggregate or disaggregate
+  if(!is.null(agg) & !is.null(disagg)){warning("both agg and disagg were provided. Did you mean to do this? (if so, note that aggregation will occur first and then disaggregation second")}
   if(!is.null(agg)) r <- raster::aggregate(r, agg)
   if(!is.null(disagg)) r <- raster::disaggregate(r, disagg)
 
