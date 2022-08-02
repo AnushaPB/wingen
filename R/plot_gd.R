648d271dc0048db748adc01e49f8699dedb5c6f1
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-01 17:47:59 -0700

    fix warnings

--- R/plot_gd.R
+++ R/plot_gd.R
@@ -4,7 +4,7 @@
 #' @param bkg RasterLayer or other spatial object that will be plotted as the "background" in gray
 #' @param col color pallete to use for plotting (defaults to viridis::magma pallete)
 #' @param breaks number of breaks to use in color scale (defaults to 10)
-#' @param index if RasterStack is provided, index of the sample count layer to plot (defaults to 1)
+#' @param index if RasterStack is provided, index of the sample count layer to plot (defaults to plotting first layer)
 #' @param zlim limits of the color scale values
 #' @inheritParams raster::plot
 #'
@@ -12,8 +12,9 @@
 #' @export
 #'
 #' @examples
-plot_gd <- function(x, bkg = NULL, index = 1, col = viridis::magma(breaks), breaks = 10, zlim = NULL, main = NULL, legend = TRUE, legend.width = 1, axis.args = list(cex.axis = 1)){
-  if(raster::nlayers(x) > 2) warning("More than two raster layers in stack provided, plotting first layer  (to change this behavior use the index argument)")
+plot_gd <- function(x, bkg = NULL, index = NULL, col = viridis::magma(breaks), breaks = 10, zlim = NULL, main = NULL, legend = TRUE, legend.width = 1, axis.args = list(cex.axis = 1)){
+  if(is.null(index) & raster::nlayers(x) > 2) warning("More than two raster layers in stack provided, plotting first layer (to change this behavior use the index argument)")
+  if(is.null(index)) index <- 1
 
   # suppress irrelevant plot warnings
   suppressWarnings({
@@ -93,7 +94,7 @@ plot_gd_bkg <- function(index, x, bkg = NULL, col = viridis::magma(breaks), brea
 #' Plot sample counts
 #'
 #' @param x RasterLayer of counts or RasterStack where indexed layer is sample counts
-#' @param index if RasterStack is provided, index of the sample count layer to plot (assumes this is a stacked output from window_gd and defaults to 2)
+#' @param index if RasterStack is provided, index of the sample count layer to plot (assumes this is a stacked output from window_gd and defaults to plotting second layer)
 #' @param col color pallete to use for plotting (defaults to viridis::magma pallete)
 #' @param breaks number of breaks to use in color scale (defaults to 10)
 #' @param zlim limits of the color scale values
@@ -103,9 +104,10 @@ plot_gd_bkg <- function(index, x, bkg = NULL, col = viridis::magma(breaks), brea
 #' @export
 #'
 #' @examples
-plot_count <- function(x, index = 2, breaks = 10, col = viridis::mako(breaks), zlim = NULL, main = NULL, legend = TRUE, legend.width = 1, axis.args = list(cex.axis = 1)){
+plot_count <- function(x, index = NULL, breaks = 10, col = viridis::mako(breaks), zlim = NULL, main = NULL, legend = TRUE, legend.width = 1, axis.args = list(cex.axis = 1)){
 
-  if(raster::nlayers(x) > 2) warning("More than two raster layers in stack provided, plotting second layer (to change this behavior use the index argument)")
+  if(is.null(index) & raster::nlayers(x) > 2) warning("More than two raster layers in stack provided, plotting second layer (to change this behavior use the index argument)")
+  if(is.null(index)) index <- 2
 
   # suppress annoying and irrelevant plot warnings
   suppressWarnings({
