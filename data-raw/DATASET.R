798790c4d229e03fb61fa16fe79f39576c5200b5
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-14 00:37:49 -0700

    update data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,9 +7,10 @@ raster::extent(lotr_lyr) <- raster::extent(0, 100, -100, 0)
 usethis::use_data(lotr_lyr, overwrite = TRUE)
 
 # make a fake range map
-lotr_lyr[lotr_lyr < 0.01] <- NA
-lotr_lyr <- lotr_lyr * 0
-lotr_range <- raster::rasterToPolygons(lotr_lyr, dissolve = TRUE, n = 16)
+lotr_range <- lotr_lyr
+lotr_range[lotr_range < 0.01] <- NA
+lotr_range <- lotr_range * 0
+lotr_range <- raster::rasterToPolygons(lotr_range, dissolve = TRUE, n = 16)
 usethis::use_data(lotr_range, overwrite = TRUE)
 
 # load coords
@@ -44,6 +45,16 @@ mini_lyr <- raster::aggregate(lotr_lyr, 10)
 mini_vcf <- lotr_vcf[1:10, 1:11]
 mini_coords <- lotr_coords[1:10, ]
 
+# add NAs to vcf for testing
+mini_vcf_NA <- mini_vcf
+mini_vcf_NA@gt[, 10] <- NA
+mini_vcf_NA@gt[9, ] <- NA
+mini_vcf_NA@gt[8, c(1, 2, 3)] <- NA
+mini_vcf_NA@gt[c(2, 3, 4), 8] <- NA
+# complete format column
+mini_vcf_NA@gt[, 1] <- "GT"
+
 usethis::use_data(mini_lyr, overwrite = TRUE)
 usethis::use_data(mini_vcf, overwrite = TRUE)
 usethis::use_data(mini_coords, overwrite = TRUE)
+usethis::use_data(mini_vcf_NA, overwrite = TRUE)
