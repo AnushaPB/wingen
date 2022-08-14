08cdbfccbb886aba935736276342b7eb0fe14352
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-14 15:41:12 -0700

    restructure convert vcf

--- R/convert_vcf.R
+++ R/convert_vcf.R
@@ -41,27 +41,15 @@ vcf_to_genind <- function(x, pops = NULL, warning = FALSE) {
   # convert to genind
   genind <- vcfR::vcfR2genind(vcf)
 
-  # TODO: Clean this up - Check if pops is false
-  if (is.logical(pops)) {
-    if (!pops) {
-      return(genind)
-    }
-  }
+
+  # leave pops NULL if pops is FALSE
+  if (is.logical(pops)) if (!pops) return(genind)
 
   # assign pops if null or pop vector provided
-  if (is.null(genind$pop) | is.vector(pops)) {
-    if (is.null(pops)) {
-      genind$pop <- as.factor(1:nrow(genind@tab))
-    }
-
-    if (is.vector(pops)) {
-      if (length(pops) != nrow(genind@tab)) {
-        stop("length of pops does not match number of individuals in genind")
-      } else {
+  if (is.null(pops)) genind$pop <- as.factor(1:nrow(genind@tab))
+  if (is.vector(pops))
+    if (length(pops) != nrow(genind@tab)) stop("length of pops does not match number of individuals in genind")
         genind$pop <- as.factor(pops)
-      }
-    }
-  }
 
   return(genind)
 }
@@ -80,12 +68,15 @@ vcf_to_het <- function(x) {
   vcf <- vcf_check(x)
 
   het <- vcfR::is.het(vcfR::extract.gt(vcf), na_is_false = FALSE)
+
   # IMPORTANT: transform matrix so that rows are individuals and cols are loci
   het <- t(het)
+
   # if gen is a vector of only one locus, turn into matrix with one column
   if (nrow(vcf@gt) == 1) {
     het <- matrix(het, ncol = 1)
   }
+
   return(het)
 }
 
@@ -99,16 +90,19 @@ vcf_to_het <- function(x) {
 #' @keywords internal
 #' @noRd
 vcf_check <- function(x) {
-  if (class(x)[1] == "vcfR") {
+  if (class(x)[1] == "vcfR")
+
     vcf <- x
-  } else if (is.character(x)) {
-    if (file.exists(x)) {
+
+  else if (is.character(x)) {
+
+    if (file.exists(x))
       vcf <- vcfR::read.vcfR(x)
-    } else {
+
+    else
       stop("Cannot open file: No such file or directory")
-    }
-  } else {
-    stop("Input is expected to be an object of class 'vcfR' or a path to a .vcf file")
-  }
+
+  } else stop("Input is expected to be an object of class 'vcfR' or a path to a .vcf file")
+
   return(vcf)
 }
