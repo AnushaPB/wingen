a91f2970f2180ecd5a3ce688d65fcb24f90fc06f
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-20 14:25:44 -0700

    change loci to variants

--- R/middle_earth.R
+++ R/middle_earth.R
@@ -20,7 +20,7 @@ load_middle_earth_ex <- function(quiet = FALSE) {
       crayon::cyan(crayon::bold("\n-------------- middle earth example --------------\n")),
       crayon::silver("\nObjects loaded:"),
       crayon::yellow(crayon::bold("\n*lotr_vcf*")),
-      crayon::yellow(paste0("vcfR object (100 loci x 100 samples)")),
+      crayon::yellow(paste0("vcfR object (100 variants x 100 samples)")),
       crayon::green(crayon::bold("\n*lotr_coords*")), crayon::green("dataframe with x and y coordinates"),
       crayon::magenta(crayon::bold("\n*lotr_lyr*")), crayon::magenta("middle earth RasterLayer (100 x 100)"),
       crayon::blue(crayon::bold("\n*lotr_range*")), crayon::blue("SpatialPolygonsDataFrame of spp range"),
@@ -50,8 +50,8 @@ load_mini_ex <- function(quiet = FALSE) {
     message(cat(
       crayon::cyan(crayon::bold("\n---------- mini middle earth example ----------\n")),
       crayon::blue("\nObjects loaded:"),
-      crayon::yellow(crayon::bold("\n*mini_vcf*")), crayon::yellow("vcfR object (10 loci x 10 samples) and no missing data"),
-      crayon::yellow(crayon::bold("\n*mini_vcf_NA*")), crayon::yellow("vcfR object (10 loci x 10 samples) and missing data"),
+      crayon::yellow(crayon::bold("\n*mini_vcf*")), crayon::yellow("vcfR object (10 variants x 10 samples) and no missing data"),
+      crayon::yellow(crayon::bold("\n*mini_vcf_NA*")), crayon::yellow("vcfR object (10 variants x 10 samples) and missing data"),
       crayon::green(crayon::bold("\n*mini_coords*")), crayon::green("dataframe with x and y coordinates"),
       crayon::magenta(crayon::bold("\n*mini_lyr*")), crayon::magenta("middle earth RasterLayer (10 x 10)"),
       crayon::cyan(crayon::bold("\n\n-----------------------------------------------"))
