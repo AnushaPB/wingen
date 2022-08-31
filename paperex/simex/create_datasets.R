4e78828ca43b8eb15684f333d5b9da2783d5e2aa
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-08-31 16:34:58 -0700

    update create datasets

--- paperex/simex/create_datasets.R
+++ paperex/simex/create_datasets.R
@@ -1,7 +1,7 @@
 
 # load genetic data ---------------------------------------------------------------------------------------------------
 
-# check if file exists locally and if not download it from figshare
+# check if the vcf file from the simulations exists locally and if not download it from figshare
 file <- here::here("paperex", "simex", "data", "mod-sim_params_it-0_t-1000_spp-spp_0.vcf")
 if(!file.exists(file)){
   message("downloading vcf from figshare and storing locally...this will take some time, but only has to be done once")
