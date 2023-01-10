ee03ed926b6c008e6de158cb97d776f83feb83ff
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-01-09 20:13:03 -0800

    simplify get_adj function

--- R/window_gd.R
+++ R/window_gd.R
@@ -442,9 +442,7 @@ get_adj <- function(i, r, n, coord_cells) {
   # get adjacent cells to cell i
   adjc <- raster::adjacent(r, i, directions = n, include = TRUE, sorted = TRUE)
   # get indices of adjacent cells
-  adjci <- purrr::map_dbl(adjc, 1, function(x) {
-    seq(x[1], x[2])
-  })
+  adjci <- purrr::map_dbl(adjc, 1, ~ seq(.x[1], .x[2]))
   # get list of indices of coords in that set of cells
   sub <- which(coord_cells %in% adjci)
 
