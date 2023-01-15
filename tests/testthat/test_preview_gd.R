836098cc8f26487b17403b5fa468d100762acc23
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-01-14 17:36:48 -0800

    Update test_preview_gd.R

--- tests/testthat/test_preview_gd.R
+++ tests/testthat/test_preview_gd.R
@@ -1,6 +1,7 @@
 test_that("preview_gd returns expected output", {
   load_mini_ex(quiet = TRUE)
-  pw <- preview_gd(mini_lyr,
+  pw <- preview_gd(
+    mini_lyr,
     mini_coords,
     wdim = 3,
     fact = 3,
@@ -8,9 +9,10 @@ test_that("preview_gd returns expected output", {
     min_n = 2
   )
 
-  expect_equal(raster::nlayers(pw), 1)
+  expect_equal(terra::nlyr(pw), 1)
 
-  pw <- preview_gd(mini_lyr,
+  pw <- preview_gd(
+    mini_lyr,
     mini_coords,
     wdim = 3,
     fact = 3,
