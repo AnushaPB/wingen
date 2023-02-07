65425fafdb84c72062b6fedeade35cfe98901645
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-02-07 00:01:21 -0800

    Update test_window_gd.R

--- tests/testthat/test_window_gd.R
+++ tests/testthat/test_window_gd.R
@@ -9,19 +9,15 @@ test_that("window_gd returns expected output", {
 
 test_that("all stats and parallel works", {
   load_mini_ex(quiet = TRUE)
-  capture_warnings(wp <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "pi", rarify = FALSE))
-  capture_warnings(wh <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "Ho", rarify = FALSE))
-  capture_warnings(wb <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "biallelic_richness", rarify = FALSE, rarify_alleles = FALSE))
-  capture_warnings(wbr <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "biallelic_richness", rarify = FALSE, rarify_alleles = TRUE))
-  capture_warnings(wa <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "allelic_richness", rarify = FALSE))
+
+  stat <- c("pi", "Ho", "biallelic_richness", "allelic_richness", "basic_stats", "hwe")
+
+  capture_warnings(wg <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat, rarify = FALSE))
 
   # check parallel
-  capture_warnings(wpp <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "pi", rarify = FALSE, parallel = TRUE, ncores = 2))
-  capture_warnings(whp <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "Ho", rarify = FALSE, parallel = TRUE, ncores = 2))
-  capture_warnings(wbp <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "biallelic_richness", rarify = FALSE, parallel = TRUE, ncores = 2))
-  capture_warnings(wap <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat = "allelic_richness", rarify = FALSE, parallel = TRUE, ncores = 2))
+  capture_warnings(wgp <- window_gd(mini_vcf_NA, mini_coords, mini_lyr, stat, rarify = FALSE, parallel = TRUE, ncores = 2))
 
-  expect_true(terra::all.equal(wpp, wp))
+  expect_true(terra::all.equal(wg, wgp))
 })
 
 test_that("rarifaction works for all options", {
