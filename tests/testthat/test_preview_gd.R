9e77f81e0a7ae0f1506d11d03efebd29ae223285
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-02-26 15:31:50 -0800

    Update test_preview_gd.R

--- tests/testthat/test_preview_gd.R
+++ tests/testthat/test_preview_gd.R
@@ -3,6 +3,7 @@ test_that("preview_gd returns expected output", {
   pw <- preview_gd(
     mini_lyr,
     mini_coords,
+    method = "window",
     wdim = 3,
     fact = 3,
     sample_count = TRUE,
@@ -54,6 +55,121 @@ test_that("preview_gd works for different coord types", {
     coords_vect,
     wdim = 3,
     fact = 3,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  expect_true(terra::all.equal(pw1, pw2))
+  expect_true(terra::all.equal(pw1, pw3))
+})
+
+
+test_that("preview_gd works for circle method with all different coordinate types", {
+  load_mini_ex(quiet = TRUE)
+  distmat <- get_geodist(mini_coords, mini_lyr, fact = 3)
+  pw <- preview_gd(
+    mini_lyr,
+    mini_coords,
+    method = "circle",
+    maxdist = 50,
+    distmat = distmat,
+    fact = 3,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  coords_sf <- sf::st_as_sf(mini_coords, coords = c("x", "y"))
+  coords_mat <- as.matrix(mini_coords)
+  coords_vect <- terra::vect(coords_sf)
+
+  par(mfrow = c(1, 3))
+  pw1 <- preview_gd(
+    mini_lyr,
+    coords_sf,
+    method = "circle",
+    maxdist = 50,
+    distmat = distmat,
+    fact = 3,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  pw2 <- preview_gd(
+    mini_lyr,
+    coords_mat,
+    method = "circle",
+    maxdist = 50,
+    distmat = distmat,
+    fact = 3,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  pw3 <- preview_gd(
+    mini_lyr,
+    coords_vect,
+    method = "circle",
+    maxdist = 50,
+    distmat = distmat,
+    fact = 3,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  expect_true(terra::all.equal(pw1, pw2))
+  expect_true(terra::all.equal(pw1, pw3))
+
+})
+
+
+test_that("preview_gd works for resist method with all different coordinate types", {
+  load_mini_ex(quiet = TRUE)
+  distmat <- get_resdist(mini_coords, mini_lyr, fact = 5, ncores = 2)
+  pw <- preview_gd(
+    mini_lyr,
+    mini_coords,
+    method = "resist",
+    maxdist = 50,
+    distmat = distmat,
+    fact = 5,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  coords_sf <- sf::st_as_sf(mini_coords, coords = c("x", "y"))
+  coords_mat <- as.matrix(mini_coords)
+  coords_vect <- terra::vect(coords_sf)
+
+  par(mfrow = c(1, 3))
+  pw1 <- preview_gd(
+    mini_lyr,
+    coords_sf,
+    method = "resist",
+    maxdist = 10,
+    distmat = distmat,
+    fact = 5,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  pw2 <- preview_gd(
+    mini_lyr,
+    coords_mat,
+    method = "resist",
+    maxdist = 10,
+    distmat = distmat,
+    fact = 5,
+    sample_count = TRUE,
+    min_n = 2
+  )
+
+  pw3 <- preview_gd(
+    mini_lyr,
+    coords_vect,
+    method = "resist",
+    maxdist = 10,
+    distmat = distmat,
+    fact = 5,
     sample_count = TRUE,
     min_n = 2
   )
