dfa984101c819c52eb823306b234e3c4836db2b0
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-02-25 16:51:20 -0800

    Update coords_to_sf.R

--- R/coords_to_sf.R
+++ R/coords_to_sf.R
@@ -2,7 +2,7 @@
 coords_to_sf <- function(coords){
   if(is.matrix(coords)) coords <- data.frame(coords)
   colnames(coords) <- c("x", "y")
-  sf_coords <- st_as_sf(coords, coords = c("x", "y"))
+  sf_coords <- sf::st_as_sf(coords, coords = c("x", "y"))
   return(sf_coords)
 }
 
