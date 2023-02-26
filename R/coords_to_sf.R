5c02902f5f86d0e4df25bc285e18ad546b6b132a
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-02-26 15:31:05 -0800

    fix coord conversion to work for vect

--- R/coords_to_sf.R
+++ R/coords_to_sf.R
@@ -1,6 +1,6 @@
 coords_to_sf <- function(coords) {
+  if (inherits(coords, "SpatVector")) return(sf::st_as_sf(coords))
   if (is.matrix(coords)) coords <- data.frame(coords)
-  colnames(coords) <- c("x", "y")
-  sf_coords <- sf::st_as_sf(coords, coords = c("x", "y"))
-  return(sf_coords)
+  if (is.data.frame(coords)) colnames(coords) <- c("x", "y")
+  return(sf::st_as_sf(coords, coords = c("x", "y")))
 }
