be8f7e0434d21f2a20c6ad9c42c481a7dfbf74e2
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-03-13 18:08:43 -0700

    Update ggplot_gd.R

--- R/ggplot_gd.R
+++ R/ggplot_gd.R
@@ -3,128 +3,110 @@
 #' Plot genetic diversity layer produced by \link[wingen]{window_gd} or \link[wingen]{krig_gd}
 #'
 #' @param x output from \link[wingen]{window_gd} or \link[wingen]{krig_gd} (RasterStack where first layer is genetic diversity)
-#' @param bkg optional RasterLayer or other spatial object that will be plotted as the "background" in gray
-#' @param col color pallete to use for plotting (defaults to viridis::magma pallete)
-#' @param breaks number of breaks to use in color scale (defaults to 10)
-#' @param index if RasterStack is provided, index of the sample count layer to plot (defaults to plotting first layer)
-#' @inheritParams raster::plot
+#' @param bkg optional raster or sf polygon
+#' @param col color pallete to use for plotting (defaults to \link[viridis]{magma} pallete)
+#' @param index index of raster layer to plot (defaults to plotting all of the layers except the one called "sample_count", if more than one layer is provided)
 #'
-#' @return plot of genetic diversity
+#' @return list of ggplots
 #' @export
 #'
 #' @examples
 #' data("mini_lyr")
-#' plot_gd(mini_lyr)
+#' ggplot_gd(mini_lyr)
 #'
-ggplot_gd <- function(x, bkg = NULL, index = NULL, col = viridis::magma(breaks), breaks = 20, main = NULL, box = FALSE, ...) {
+ggplot_gd <- function(x, bkg = NULL, index = NULL, col = viridis::magma(100), ...) {
 
-  if (is.null(index) & terra::nlyr(x) > 2) warning("More than two raster layers in stack provided, plotting first layer (to change this behavior use the index argument)")
-  if (is.null(index)) index <- 1
+  if (!inherits(x, "SpatRaster")) x <- terra::rast(x)
+  if (is.null(index) & terra::nlyr(x) > 2) x <- terra::subset(x, "sample_count", negate = TRUE)
 
-  # suppress irrelevant plot warnings
-  suppressWarnings({
-    if (!is.null(bkg)) {
-      plt <- purrr::map(index, plot_gd_bkg, x = x, bkg = bkg, col = col, breaks = breaks, main = main, box = box, ...)
-    } else {
-
-      plt <- terra::plot(x[[index]],
-                          col = col,
-                          axes = FALSE,
-                          box = box,
-                          ...
-      )
-      title(main = list(main, font = 1), adj = 0)
-    }
-  })
-
-  return(invisible(plt))
-}
-
-#' Helper function for plot_gd
-#'
-#' @inheritParams plot_gd
-#'
-#' @export
-#' @noRd
-ggplot_gd_bkg <- function(index, x, bkg = NULL, col = viridis::magma(breaks), breaks = 20, main = NULL, box = FALSE, ...) {
-
+  # make df
   x_df <- x[[index]] %>%
     terra::as.data.frame(xy = TRUE) %>%
     tidyr::as_tibble()
-  colnames(x_df) <- c("x", "y", "layer")
 
-  if (!is.null(breaks)) x_df <- x_df  %>% dplyr::mutate(layer = dplyr::ntile(layer, n = breaks))
+  # plot results
+  plts <-
+    x_df  %>%
+    dplyr::select(-x, -y) %>%
+    purrr::imap(\(var, i) ggplot_helper(var = var, i = i, x_df = x_df, col = col, bkg = bkg))
 
-  gg <- ggplot2::ggplot()
-  if (!is.null(bkg)) gg <- gg + ggplot2::geom_polygon(data = bkg, ggplot2::aes(x = long, y = lat, group = group), fill = "lightgray")
+  purrr::walk(plts, print)
 
-  # TODO: change so it is more cusotomizable
-  # TODO: change so legend isn't cramped
-  gg <- gg +
-    # change so works if bkg is a raster
-    ggplot2::geom_tile(data = x_df, ggplot2::aes(x = x, y = y, fill = layer)) +
-    ggplot2::coord_fixed() +
-    ggplot2::theme_bw() +
-    #ggplot2::scale_fill_stepsn(n.breaks = breaks, colours = col) +
-    ggplot2::scale_fill_viridis_c(n.breaks = 20) +
-    ggplot2::theme(panel.grid.minor.y = ggplot2::element_blank(),
-                   panel.grid.major.y = ggplot2::element_blank(),
-                   panel.grid.minor.x = ggplot2::element_blank(),
-                   panel.grid.major.x = ggplot2::element_blank(),
-                   axis.title.x = ggplot2::element_blank(),
-                   axis.text.x = ggplot2::element_blank(),
-                   axis.ticks.x = ggplot2::element_blank(),
-                   axis.title.y = ggplot2::element_blank(),
-                   axis.text.y = ggplot2::element_blank(),
-                   axis.ticks.y = ggplot2::element_blank(),
-                   panel.border = ggplot2::element_blank())
-
-  return(gg)
+  return(plts)
 }
+
 
 #' Plot moving window map of sample counts
 #'
 #' Plot sample counts layer produced by \link[wingen]{window_gd} or \link[wingen]{krig_gd}
 #'
-#' @param x RasterLayer of counts or RasterStack where indexed layer is sample counts
-#' @param index if RasterStack is provided, index of the sample count layer to plot (assumes this is a stacked output from window_gd and defaults to plotting second layer)
-#' @param col color pallete to use for plotting (defaults to viridis::magma pallete)
-#' @param breaks number of breaks to use in color scale (defaults to 10)
-#' @inheritParams plot_gd
-#' @inheritParams raster::plot
+#' @param x single SpatRaster of counts or SpatRaster where indexed layer is sample counts
+#' @param index if a raster stack is provided, index of the sample count layer to plot
+#' (assumes this is a stacked output from window_gd and defaults to plotting the last layer (which should be sample counts))
+#' @param col color palette to use for plotting (defaults to viridis::magma palette)
 #'
-#' @return plot of sample counts
+#' @return list of ggplots
 #' @export
 #'
 #' @examples
 #' data("mini_lyr")
 #' plot_count(mini_lyr)
-ggplot_count <- function(x, index = NULL, breaks = 20, col = viridis::mako(breaks), main = NULL, box = FALSE, ...) {
-  if (is.null(index) & raster::nlayers(x) > 2) warning("More than two raster layers in stack provided, plotting second layer (to change this behavior use the index argument)")
-  if (is.null(index)) index <- 2
+ggplot_count <- function(x, index = NULL, breaks = 100, col = viridis::mako(breaks), ...) {
+  if (!inherits(x, "SpatRaster")) x <- terra::rast(x)
+  if (is.null(index) & terra::nlyr(x) > 2) warning("More than two raster layers in stack provided, plotting second layer (to change this behavior use the index argument)")
+  if (is.null(index)) index <- terra::nlyr(x)
 
-  # suppress annoying and irrelevant plot warnings
-  suppressWarnings({
-    if (raster::nlayers(x) > 1) {
-      plt <- raster::plot(x[[index]],
-        col = col,
-        axes = FALSE,
-        box = box,
-        ...
-      )
-      title(main = list(main, font = 1), adj = 0)
+  # make df
+  x_df <- x[[index]] %>%
+    terra::as.data.frame(xy = TRUE) %>%
+    tidyr::as_tibble()
+
+  # plot results
+  plts <-
+    x_df  %>%
+    dplyr::select(-x, -y) %>%
+    purrr::imap(\(var, i) ggplot_helper(var = var, i = i, x_df = x_df, col = col, bkg = bkg))
+
+  purrr::walk(plts, print)
+
+  return(plts)
+}
+
+#' Helper function for ggplot_gd and ggplot_count
+#'
+#' @param var variable to plot
+#' @param x_df ggplot dataframe
+#' @param bkg background raster layer or sf objects
+#' @return ggplot
+#' @noRd
+ggplot_helper <- function(var, i, x_df, col, bkg = NULL){
+  # create ggplot
+  gg <- ggplot2::ggplot()
+
+  # add background
+  if (!is.null(bkg)) {
+    if (inherits(bkg, "sf")) gg <- gg + ggplot2::geom_sf(data = bkg, fill = "lightgray")
+    if (inherits(bkg, "SpatRaster")) {
+      bkg_df <- bkg %>%
+        terra::as.data.frame(xy = TRUE) %>%
+        tidyr::as_tibble()
+      gg <- gg + ggplot2::geom_tile(data = bkg_df, ggplot2::aes(x = x, y = y), fill = "lightgray")
     }
 
-    if (raster::nlayers(x) == 1) {
-      plt <- raster::plot(x,
-        col = col,
-        axes = FALSE,
-        box = box,
-        ...
-      )
-      title(main = list(main, font = 1), adj = 0)
-    }
-  })
+  }
 
-  return(invisible(plt))
+  # plot result
+  gg <- gg +
+    ggplot2::geom_tile(data = x_df, ggplot2::aes(x = x, y = y, fill = {{var}})) +
+    ggplot2::theme_bw() +
+    ggplot2::scale_fill_gradientn(colours = col, na.value = rgb(0,0,0,0)) +
+    #ggplot2::labs(fill = deparse(substitute(var))) +
+    ggplot2::labs(fill = i) +
+    ggplot2::theme(panel.grid = ggplot2::element_blank(),
+                   axis.title = ggplot2::element_blank(),
+                   axis.text = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank())
+
+  return(gg)
 }
