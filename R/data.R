178b7f7a9b00e21e83b4837aba1fe9eb4f6a6e67
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-06-12 14:12:20 -0700

    add new data object and update docs

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #'   \item{x}{x coordinate}
 #'   \item{y}{y coordinate}
 #' }
-#' @source created from simulations in Bishop et al. (in review)
+#' @source created from simulations in Bishop et al. (2023)
 "lotr_coords"
 
 #' Middle earth example vcf
@@ -13,7 +13,7 @@
 #' A Variant Call Format data set
 #'
 #' @format Object of class vcfR with 100 individuals and 1000 loci
-#' @source created from simulations in Bishop et al. (in review)
+#' @source created from simulations in Bishop et al. (2023)
 "lotr_vcf"
 
 #' Middle earth example raster
@@ -23,9 +23,18 @@
 #' ([Robert, 2020](https://scholarworks.wm.edu/asoer/3/)).
 #'
 #' @format RasterLayer
-#' @source created from simulations in Bishop et al. (in review) based on Rose, Robert A. (2020) GIS & Middle Earth Presentation & Data Set. William & Mary. https://doi.org/10.21220/RKEZ-X707
+#' @source created from simulations in Bishop et al. (2023) based on Rose, Robert A. (2020) GIS & Middle Earth Presentation & Data Set. William & Mary. https://doi.org/10.21220/RKEZ-X707
 #'
 "lotr_lyr"
+
+#' Middle earth example resistance distance matrix
+#'
+#' Resistance distance matrix made based on \link[wingen]{lotr_lyr} aggregated by a factor of 5 and \link[wingen]{lotr_coords}
+#'
+#' @format matrix
+#' @source created from simulations in Bishop et al. (2023)
+#'
+"lotr_distmat"
 
 #' Middle earth example range polygon
 #'
@@ -33,7 +42,7 @@
 #'
 #'
 #' @format sf
-#' @source  created from simulations in Bishop et al. (in review)
+#' @source  created from simulations in Bishop et al. (2023)
 "lotr_range"
 
 
@@ -44,7 +53,7 @@
 #'   \item{x}{x coordinate}
 #'   \item{y}{y coordinate}
 #' }
-#' @source created from simulations in Bishop et al. (in review)
+#' @source created from simulations in Bishop et al. (2023)
 "mini_coords"
 
 #' Mini middle earth example vcf
