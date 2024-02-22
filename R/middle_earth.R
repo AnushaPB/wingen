91cb2e5dd02d077e6bd0a70b5575a85150f7d13e
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-02-21 21:49:08 -0800

    update message

--- R/middle_earth.R
+++ R/middle_earth.R
@@ -18,11 +18,11 @@ load_middle_earth_ex <- function(quiet = FALSE) {
     return(message(
       crayon::cyan(crayon::bold("\n-------------- middle earth example --------------\n")),
       crayon::silver("\nObjects loaded:"),
-      crayon::yellow(crayon::bold("\n*lotr_vcf*")),
+      crayon::yellow(crayon::bold("\nlotr_vcf: ")),
       crayon::yellow(paste0("vcfR object (100 variants x 100 samples)")),
-      crayon::green(crayon::bold("\n*lotr_coords*")), crayon::green("dataframe with x and y coordinates"),
-      crayon::magenta(crayon::bold("\n*lotr_lyr*")), crayon::magenta("middle earth RasterLayer (100 x 100)"),
-      crayon::blue(crayon::bold("\n*lotr_range*")), crayon::blue("SpatialPolygonsDataFrame of spp range"),
+      crayon::green(crayon::bold("\nlotr_coords: ")), crayon::green("dataframe with x and y coordinates"),
+      crayon::magenta(crayon::bold("\nlotr_lyr: ")), crayon::magenta("middle earth RasterLayer (100 x 100)"),
+      crayon::blue(crayon::bold("\nlotr_range: ")), crayon::blue("SpatialPolygonsDataFrame of spp range"),
       crayon::cyan(crayon::bold("\n\n--------------------------------------------------\n"))
     ))
   }
@@ -46,13 +46,13 @@ load_mini_ex <- function(quiet = FALSE) {
   # give message with information about objects
   if (!quiet) {
     message(
-      crayon::cyan(crayon::bold("\n---------- mini middle earth example ----------\n")),
+      crayon::cyan(crayon::bold("\n-------------------- mini middle earth example --------------------\n")),
       crayon::blue("\nObjects loaded:"),
-      crayon::yellow(crayon::bold("\n*mini_vcf*")), crayon::yellow("vcfR object (10 variants x 10 samples) and no missing data"),
-      crayon::yellow(crayon::bold("\n*mini_vcf_NA*")), crayon::yellow("vcfR object (10 variants x 10 samples) and missing data"),
-      crayon::green(crayon::bold("\n*mini_coords*")), crayon::green("dataframe with x and y coordinates"),
-      crayon::magenta(crayon::bold("\n*mini_lyr*")), crayon::magenta("middle earth RasterLayer (10 x 10)"),
-      crayon::cyan(crayon::bold("\n\n-----------------------------------------------"))
+      crayon::yellow(crayon::bold("\nmini_vcf: ")), crayon::yellow("vcfR object (10 variants x 10 samples) and no missing data"),
+      crayon::yellow(crayon::bold("\nmini_vcf_NA: ")), crayon::yellow("vcfR object (10 variants x 10 samples) and missing data"),
+      crayon::green(crayon::bold("\nmini_coords: ")), crayon::green("dataframe with x and y coordinates"),
+      crayon::magenta(crayon::bold("\nmini_lyr: ")), crayon::magenta("middle earth RasterLayer (10 x 10)"),
+      crayon::cyan(crayon::bold("\n\n-------------------------------------------------------------------"))
     )
   }
 }
